10023c93df41829f80606e53f3609d182bd4dd36
rylienellis <57364564+rylienellis@users.noreply.github.com>
2020-02-19 11:44:30 -0500

    Update defaults.js

--- week6/storymap-basic-master/config/defaults.js
+++ week6/storymap-basic-master/config/defaults.js
@@ -25,7 +25,7 @@ define({
   "group": "",
   //Enter the url to the proxy if needed by the application. See the 'Using the proxy page' help topic for details
   //http://developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
-  "proxyurl": "",
+  "proxyurl": "https://luna.flemingcollege.ca/arcgis/rest/services/Geom99on/rnellisCanadaPro/MapServer/0?token=Ahy6jj7JkOz5r43mCQkoFXr3e3Lmvm2tRsiOCJU5YapcBXJzdAIeL9j4K3RG6qCGZ7K9PEa4ivkhsYNh1uTGrnFvEZeDNP6iABHTNjvaRrPKMIGxcGe8fnjKVWG5WQAHNLMhBva57HaBWYeRoHzbeA..",
   // This is an option added so that developers working with the
   // hosted version of the application can apply custom styles
   // not used in the download version.
